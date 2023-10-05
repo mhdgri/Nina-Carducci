88f6b6c00424077ec3a395dba8dcb53259fbcec0
GUERROUI Mohamed <mohamed.guerroui.mg@gmail.com>
2023-10-05 13:53:07 +0200

    deployment

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,3 +13,15 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
+
+//service worker
+
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+    console.log('Service Worker enregistré avec succès:', registration);
+    })
+    .catch(function(error) {
+    console.log("Échec de l'enregistrement du Service Worker:", error);
+    });
+}
--- sw.js
+++ sw.js
@@ -1,8 +1,35 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker installé.');
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('mon-cache-v1')
+            .then(function(cache) {
+                return cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/assets/style.min.css',
+                    '/assets/scripts.min.js',
+                    '/assets/maugallery.min.js',
+                    '/assets/bootstrap/bootstrap.bundle.min.js',
+                    '/assets/bootstrap/bootstrap.min.css',
+                    '/assets/images/maskable_icon-192x192.png',
+                    '/assets/images/maskable_icon-512x512.png',
+                    '/manifest.json'
+                ]);
+            })
+            .catch(function(error) {
+                console.error("Erreur lors de l'ouverture du cache : ", error);
+            })
+    );
 });
 
-self.addEventListener('fetch', event => {
-    console.log('Fetch intercepted for:', event.request.url);
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+            .catch(function(error) {
+                console.error("Erreur lors de la récupération de la ressource : ", error);
+                return fetch(event.request);
+            })
+    );
 });
